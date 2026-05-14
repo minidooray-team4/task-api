be28b36e54347cb87bdd56e02d64aad7ce7c1c30
Quasimorphism <bak575755@gmail.com>
2026-05-14 13:29:58 +0900

    init: init

---
+++ src/test/java/com/nhnacademy/tem4/taskapi/TaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.tem4.taskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
