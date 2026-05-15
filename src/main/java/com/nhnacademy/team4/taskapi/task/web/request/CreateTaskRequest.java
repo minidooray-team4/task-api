81214d6ef152b63ab66a95d177a205270bd23786
{dabin5682} <dabin5682@gmail.com>
2026-05-15 16:50:29 +0900

    feat: Task 조회 기능 구현

--- src/main/java/com/nhnacademy/team4/taskapi/task/web/request/CreateTaskRequest.java
+++ src/main/java/com/nhnacademy/team4/taskapi/task/web/request/CreateTaskRequest.java
@@ -1,6 +1,6 @@
 package com.nhnacademy.team4.taskapi.task.web.request;
 
-import com.nhnacademy.team4.taskapi.project.domain.Project;
+
 import com.nhnacademy.team4.taskapi.task.application.command.CreateTaskCommand;
 
 public record CreateTaskRequest(
