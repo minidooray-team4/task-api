32ece74a658ce7d74b7bd9483c633930ce1f3c6f
a01095594899 <a01095594899@gmail.com>
2026-05-15 10:50:43 +0900

    fix : Project Entity pull 수정

--- src/main/java/com/nhnacademy/team4/taskapi/tasktags/domain/TaskTags.java
+++ src/main/java/com/nhnacademy/team4/taskapi/tasktags/domain/TaskTags.java
@@ -18,11 +18,11 @@ public class TaskTags {
     @Column(name = "id")
     private Long id;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @Column(name = "task_id",nullable = false)
-//    private Long taskId;
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @Column(name = "tag_id",nullable = false)
-//    private Long tagId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "task_id",nullable = false)
+    private Long taskId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "tag_id",nullable = false)
+    private Long tagId;
 }
