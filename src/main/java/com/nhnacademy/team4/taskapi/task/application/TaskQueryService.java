3d6871df3f0bf68b60a004f8171adb63ac7182eb
{dabin5682} <dabin5682@gmail.com>
2026-05-15 13:35:48 +0900

    feat : README.md 에 Usecase 추가 fix: CommentResult 를 Comment 하위로 경로변경

--- src/main/java/com/nhnacademy/team4/taskapi/task/application/TaskQueryService.java
+++ src/main/java/com/nhnacademy/team4/taskapi/task/application/TaskQueryService.java
@@ -1,21 +1,17 @@
 package com.nhnacademy.team4.taskapi.task.application;
 
+
+
 import com.nhnacademy.team4.taskapi.task.application.command.GetProjectTasksQuery;
-import com.nhnacademy.team4.taskapi.task.application.result.TaskDetailResult;
 import com.nhnacademy.team4.taskapi.task.application.result.TaskResult;
 import com.nhnacademy.team4.taskapi.task.application.result.TaskSummaryResult;
-import com.nhnacademy.team4.taskapi.task.application.usecase.GetProjectTaskUseCase;
 import com.nhnacademy.team4.taskapi.task.application.usecase.GetProjectTasksUseCase;
 import com.nhnacademy.team4.taskapi.task.application.usecase.GetTaskDetailUseCase;
 
 import java.util.List;
 
-public class TaskQueryService implements GetProjectTasksUseCase , GetProjectTaskUseCase, GetTaskDetailUseCase {
-    @Override
-    public TaskDetailResult getProjectTask(GetProjectTaskUseCase command) {
-        //미구현
-        return null;
-    }
+public class TaskQueryService implements GetProjectTasksUseCase, GetTaskDetailUseCase {
+
 
     @Override
     public List<TaskSummaryResult> getProjectTasks(GetProjectTasksQuery query) {
--- src/main/java/com/nhnacademy/team4/taskapi/task/web/response/TaskDetailResponse.java
+++ src/main/java/com/nhnacademy/team4/taskapi/task/web/response/TaskDetailResponse.java
@@ -1,6 +1,6 @@
 package com.nhnacademy.team4.taskapi.task.web.response;
 
-import com.nhnacademy.team4.taskapi.task.application.result.CommentResult;
+import com.nhnacademy.team4.taskapi.comment.application.result.CommentResult;
 import com.nhnacademy.team4.taskapi.task.application.result.TagResult;
 
 import java.util.List;
