e6bafab24331b601037d0a307c4f11175518f701
a01095594899 <a01095594899@gmail.com>
2026-05-19 15:32:35 +0900

    fix : Tag -> UseCase를 void로 반환
    # Conflicts:
    #	src/main/java/com/nhnacademy/team4/taskapi/tags/application/TagCommandService.java
    #	src/main/java/com/nhnacademy/team4/taskapi/tags/application/usecase/CreateTagUseCase.java
    #	src/main/java/com/nhnacademy/team4/taskapi/tags/application/usecase/UpdateTagUseCase.java
    #	src/main/java/com/nhnacademy/team4/taskapi/tags/web/TagController.java

--- src/main/java/com/nhnacademy/team4/taskapi/tags/application/TagCommandService.java
+++ src/main/java/com/nhnacademy/team4/taskapi/tags/application/TagCommandService.java
@@ -10,6 +10,7 @@ import com.nhnacademy.team4.taskapi.tags.application.command.CreateTagCommand;
 import com.nhnacademy.team4.taskapi.tags.application.command.DetachTagFromTaskCommand;
 import com.nhnacademy.team4.taskapi.tags.application.command.UpdateTagCommand;
 import com.nhnacademy.team4.taskapi.tags.application.result.TagResult;
+import com.nhnacademy.team4.taskapi.tags.application.usecase.*;
 import com.nhnacademy.team4.taskapi.tags.domain.Tag;
 import com.nhnacademy.team4.taskapi.tags.persistence.TagRepository;
 import com.nhnacademy.team4.taskapi.task.domain.Task;
