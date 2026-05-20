f7d871f11cc947674e54fac64ff76e5ab693955a
a01095594899 <a01095594899@gmail.com>
2026-05-20 10:15:39 +0900

    feat : test코드 생성

--- src/main/java/com/nhnacademy/team4/taskapi/tags/web/TagController.java
+++ src/main/java/com/nhnacademy/team4/taskapi/tags/web/TagController.java
@@ -25,7 +25,7 @@ public class TagController {
     private final TagQueryService tagQueryService;
 
     @PostMapping("/projects/{projectId}/tags")
-    public ResponseEntity<TagResponse> addTag(
+    public ResponseEntity<Void> addTag(
             @PathVariable Long projectId,
             @RequestHeader("X-MEMBER-ID") Long requesterMemberId,
             @RequestBody CreateTagRequest request
@@ -50,7 +50,7 @@ public class TagController {
     }
 
     @PatchMapping("/tags/{tagId}")
-    public ResponseEntity<TagResponse> updateTag(
+    public ResponseEntity<Void> updateTag(
             @PathVariable Long tagId,
             @RequestHeader("X-MEMBER-ID") Long requesterMemberId,
             @RequestBody UpdateTagRequest request
