19cad894be0fab9341b9e46047431a8d895d473e
{dabin5682} <dabin5682@gmail.com>
2026-05-22 10:54:43 +0900

    feat: Comment Controller 테스트 케이스 추가

--- src/main/java/com/nhnacademy/team4/taskapi/comment/persistence/CommentRepository.java
+++ src/main/java/com/nhnacademy/team4/taskapi/comment/persistence/CommentRepository.java
@@ -4,6 +4,8 @@ package com.nhnacademy.team4.taskapi.comment.persistence;
 import com.nhnacademy.team4.taskapi.comment.domain.Comment;
 import com.nhnacademy.team4.taskapi.task.domain.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +13,6 @@ import java.util.Optional;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByTask_Id(Long taskId);
 
-    Optional<Comment> findByWriterMemberIdAndTask_Id(Long writerMemberId, Long task_id);
+    @Query("select c from Comment c join fetch c.task where c.id = :commentId")
+    Optional<Comment> findWithTaskById(@Param("commentId") Long commentId);
 }
